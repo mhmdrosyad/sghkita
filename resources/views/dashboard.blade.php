00a486ac009010efa58237e67de148a563261fd4
Rosyad <rosyadblog@gmail.com>
2024-08-19 08:49:04 +0700

    fixed: header and sidebar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,20 +7,7 @@
                 </div>
             </div>
             <!-- end col -->
-            <div class="col-md-6">
-                <div class="breadcrumb-wrapper">
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item">
-                                <a href="#0">Dashboard</a>
-                            </li>
-                            <li class="breadcrumb-item active" aria-current="page">
-                                eCommerce
-                            </li>
-                        </ol>
-                    </nav>
-                </div>
-            </div>
+
             <!-- end col -->
         </div>
         <!-- end row -->
