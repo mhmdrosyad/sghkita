d0ac04ffd9935d5a915dd30d7fe4bbac3f39a22c
Rosyad <rosyadblog@gmail.com>
2024-09-04 11:32:25 +0700

    fixed balance in dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,9 +12,9 @@ class HomeController extends Controller
         $foAccount = Account::where('code', '101')->first();
         $bcaAccount = Account::where('code', '102')->first();
         $bcaPayroll = Account::where('code', '103')->first();
-        $foBalance = $foAccount->current_balance;
-        $bcaBalance = $bcaAccount->current_balance;
-        $bcaPayrollBalance = $bcaPayroll->current_balance;
+        $foBalance = $foAccount->current_balance ?? 0;
+        $bcaBalance = $bcaAccount->current_balance ?? 0;
+        $bcaPayrollBalance = $bcaPayroll->current_balance ?? 0;
         return view('dashboard', compact('foBalance', 'bcaBalance', 'bcaPayrollBalance'));
     }
 }
